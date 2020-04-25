c16b839cf81ccfcf8ea17b243948b763e11dc75c
DinaTaklit <ez_taklit@esi.dz>
2020-04-25 21:21:32 +0100

    update: extract utils alone for structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import utils from '../math-utils';
 
 
 // Extract stars as component and since a component should hold one sigle element and here we are using map we need to wrap it into a fragment
 const StarsDisplay = (props) => (
   <>
-    {utils.range(1, props.count).map((starId) => (
+      {utils.range(1, props.count).map((starId) => (
       <div key={starId} className="star" />
-    ))}
+      ))}
   </>
 );
+
 
 // Extract play button compoenent
 const PlayNumber = (props) => (
@@ -26,9 +28,9 @@ const PlayAgain = (props) => (
   <div className="game-done">
     <div
       className="message"
-      style={{ color: props.gameStatus === "lost" ? "red" : "green" }}
+      style={{ color: props.gameStatus === 'lost' ? 'red' : 'green' }}
     >
-      {props.gameStatus === "lost" ? "Game Over" : "You Won!"}
+      {props.gameStatus === 'lost' ? 'Game Over' : 'You Won!'}
     </div>
     <button onClick={props.onClick}> Play Again</button>
   </div>
@@ -89,7 +91,7 @@ const Game = (props) => {
 
   // check the game status after seconds left is 0
   const gameStatus =
-    availableNums.length === 0 ? "won" : secondsLeft === 0 ? "lost" : "active";
+    availableNums.length === 0 ? 'won' : secondsLeft === 0 ? 'lost' : 'active';
 
   // Reset the game one the game is done => no need anymore after using the logic of key elemnt and unmounting see StartMatch component
   // const resetGame = () => {
@@ -100,19 +102,19 @@ const Game = (props) => {
 
   const numberStatus = (number) => {
     if (!availableNums.includes(number)) {
-      return "used";
+      return 'used';
     }
     if (candidateNums.includes(number)) {
-      return candidatesAreWrong ? "wrong" : "candidate";
+      return candidatesAreWrong ? 'wrong' : 'candidate';
     }
-    return "available";
+    return 'available';
   };
   const onNumberClick = (number, currentStatus) => {
-    if (gameStatus !== "active" || currentStatus === "used") {
+    if (gameStatus !== 'active' || currentStatus === 'used') {
       return;
     }
     const newCandidateNums =
-      currentStatus === "available"
+      currentStatus === 'available'
         ? candidateNums.concat(number)
         : candidateNums.filter((cn) => cn !== number);
 
@@ -126,7 +128,7 @@ const Game = (props) => {
       </div>
       <div className="body">
         <div className="left">
-          {gameStatus !== "active" ? (
+          {gameStatus !== 'active' ? (
             <PlayAgain onClick={props.startNewGame} gameStatus={gameStatus} />
           ) : (
             <StarsDisplay count={stars}></StarsDisplay>
@@ -156,42 +158,11 @@ const StarMatch = () => {
 
 // Color Theme
 const colors = {
-  available: "lightgray",
-  used: "lightgreen",
-  wrong: "lightcoral",
-  candidate: "deepskyblue",
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
 };
-
-// Math science
-const utils = {
-  // Sum an array
-  sum: (arr) => arr.reduce((acc, curr) => acc + curr, 0),
-
-  // create an array of numbers between min and max (edges included)
-  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
-
-  // pick a random number between min and max (edges included)
-  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
-
-  // Given an array of numbers and a max...
-  // Pick a random sum (< max) from the set of all available sums in arr
-  randomSumIn: (arr, max) => {
-    const sets = [[]];
-    const sums = [];
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0, len = sets.length; j < len; j++) {
-        const candidateSet = sets[j].concat(arr[i]);
-        const candidateSum = utils.sum(candidateSet);
-        if (candidateSum <= max) {
-          sets.push(candidateSet);
-          sums.push(candidateSum);
-        }
-      }
-    }
-    return sums[utils.random(0, sums.length - 1)];
-  },
-};
-
 
 
 export default function App() {
